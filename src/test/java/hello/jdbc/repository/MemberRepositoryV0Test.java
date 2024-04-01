3f64ea32f36bad5825bade1a99d7bf8b4cae5580
jjang0308 <jang653838@naver.com>
2024-04-01 17:46:28 +0900

    delete test

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,12 +18,24 @@ class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV100", 10000);
         repository.save(member);
+
+        //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember = {}", findMember);
-        log.info("member == findMember", member == findMember );
-        log.info("member == findMember", member.equals(findMember));
         assertThat(findMember).isEqualTo(member);
+
+        //update: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(()-> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
     }
 }
