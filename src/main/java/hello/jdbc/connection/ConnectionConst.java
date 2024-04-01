8c8ee68884f7cbc2518956e6af71a5bc1477ec9b
jjang0308 <jang653838@naver.com>
2024-04-01 17:23:44 +0900

    connection, mysql로 db변경

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    static final String URL = "jdbc:mysql://localhost:3306/jdbc";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "0000";
+
+}
